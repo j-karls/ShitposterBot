13076e7ebc86a0fad99b126d926c47eb8da67f23
Unknown <Karlzone0@gmail.com>
2019-02-02 22:41:59 +0100

    Added many prototypes

--- connector.py
+++ connector.py
@@ -1,64 +1,99 @@
 # Works only with Python 3.6
 import discord
 import re
+import requests
+import random
 
-TOKEN = open('bot-token.secret', 'r').read()
-client = discord.Client()
+
+def discord_connect(token):
+    connection = discord.Client()
+    connection.run(token)
+    return connection
+
+
+def read_file(path):
+    open(path, 'r').read()
+    # We don't close the stream ourselves, garbage collector will come along shortly
+    # It may be worth doing explicitly
+
+
+client = discord_connect(read_file('bot-token.secret'))
+# Connects with the bot-token saved within file
+
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
+    # To avoid the bot to replying to itself
     if message.author == client.user:
         return
 
     if message.content.startswith('--help'):
-        msg = f'Hello {message.author.mention} \n' \
-               '\n' \
-               'Commands: \n' \
-               '--help | Opens the help menu \n' \
-               '--connections | Shows the list of subreddits connected to this Discord channel \n' \
-               '--add [subreddit] [amount] [frequency] [selection] | Connects a subreddit to the Discord channel \n' \
-               '--remove [connectionNumber] | Removes a subreddit connection \n' \
-               '\n' \
-               'Parameters: \n' \
-               'subreddit: A subreddit name \n' \
-               'amount: Amount of posts, between 0 and 25' \
-               'frequency: hourly, daily, weekly, monthly, yearly \n' \
-               'selection: top, random \n' \
-               'connectionNumber: A number found in the list of connections with command --connections'
-        await client.send_message(message.channel, msg)
+        await discord_send_message(message.channel, message_help(message.author.mention))
 
     elif message.content.startswith('--connections'):
-        msg = f'Connections:'
-        await client.send_message(message.channel, msg)
+        await discord_send_message(message.channel, message_connections(message, get_connections()))
 
     elif message.content.startswith('--add'):
-        regex = re.search(r"--add\s(\w*)\s(\w*)\s(\w*)\s(\w*)", message.content)
-        if regex:
-            subreddit = regex.group(1)
-            amount = regex.group(2)
-            frequency = regex.group(3)
-            selection = regex.group(4)
-            msg = 'Added connection: \n' \
-                 f'{subreddit} linked to channel {message.channel}. \n' \
-                 f'It will write {amount} {selection} posts every {frequency}'
-            await client.send_message(message.channel, msg)
+        try:
+            regex = regex_search(r"--add\s+(\w*)\s+(\w*)\s+(\w*)\s+(\w*)", 4, message.content)
+            subreddit, amount, frequency, selection = regex.group(1), regex.group(2), regex.group(3), regex.group(4)
+            add_connection(subreddit, amount, frequency, selection)
+            await discord_send_message(message.channel,
+                                       message_added_connection(message, subreddit, amount, frequency, selection))
+        except TypeError:
+            await discord_send_message(message.channel, message_wrong_syntax("--add"))
 
     elif message.content.startswith('--remove'):
-        regex = re.search(r"--add\s(\d*)", message.content)
-        if regex:
-            number = regex.group(1)
-            connectioninfo = "somethingsomethingredditsomething"
-            #connectioninfo = connections[number]
-            msg = f'Removed:' \
-                  f'Removed connection {connectioninfo}'
-            await client.send_message(message.channel, msg)
+        try:
+            regex = regex_search(r"--remove\s+(\d*)", 1, message.content)
+            connection_number, connection_info = regex.group(1), get_connection_info(regex.group(1))
+            remove_connection(connection_number)
+            await discord_send_message(message.channel, message_removed_connection(connection_info))
+        except TypeError:
+            await discord_send_message(message.channel, message_wrong_syntax("--remove"))
+# TODO Check if there's something behind my regex? Only if there isn't, do I execute it.
 
 
-# def getJSON:
-# import requests
-# r = requests.get(p'https://www.reddit.com/r/{subreddit}.json')
-# r.json()
+def regex_search(regex, number_of_match_groups, text):
+    res = re.search(regex, text)
+    for x in range(1, number_of_match_groups):
+        if res.group(x) is None:
+            raise TypeError
+    return res
+
+
+async def discord_send_message(channel, message):
+    await client.send_message(channel, message)
+
+
+def add_connection(subreddit, amount, frequency, selection):
+    # TODO
+
+
+def get_connection_info(connection):
+    # TODO
+
+
+def remove_connection(connection_number):
+    # TODO
+
+
+def get_connections():
+    # TODO
+
+
+def message_wrong_syntax(command_name):
+    return f"Wrong syntax for command '{command_name}'. Check '--help' for info."
+
+
+def get_json(subreddit, amount, randomize):
+    posts = requests.get(f'https://www.reddit.com/r/{subreddit}.json').json()["data"]["children"]
+    if randomize:
+        random.shuffle(posts) # CAN I DO THIS? Is it even a list?
+    return [posts[x]["data"]["url"] for x in range(amount)]
+    # TODO What happens if amount is greater than the amount of posts?
+    # TODO What happens if there is no field: "url"?
+
 
 @client.event
 async def on_ready():
@@ -67,4 +102,40 @@ async def on_ready():
     print(client.user.id)
     print('------')
 
-client.run(TOKEN)
+
+def message_help(message):
+    return f'Hello {message.author.mention} \n' \
+            '\n' \
+            'Commands: \n' \
+            '--help | Opens the help menu \n' \
+            '--connections | Shows the list of subreddits connected to this Discord channel \n' \
+            '--add [subreddit] [amount] [frequency] [selection] | Connects a subreddit to the Discord channel \n' \
+            '--remove [connectionNumber] | Removes a subreddit connection \n' \
+            '\n' \
+            'Parameters: \n' \
+            'subreddit: A subreddit name \n' \
+            'amount: Amount of posts, between 0 and 25 \n' \
+            'frequency: hourly, daily, weekly, monthly, yearly \n' \
+            'selection: top, random \n' \
+            'connectionNumber: A number found in the list of connections with command --connections'
+
+
+def message_connections(message, connections):
+    return "All connections in channel(?) and server (?)"
+    # return list of connections
+
+
+def message_added_connection(message, subreddit, amount, selection, frequency):
+    return 'Added connection: \n' \
+          f'{subreddit} linked to channel {message.channel}. \n' \
+          f'It will write {amount} {selection} posts every {frequency}'
+
+
+def message_removed_connection(connection_info):
+    return 'Removed Connection: \n' \
+          f'{connection_info}'
+
+
+
+
+
